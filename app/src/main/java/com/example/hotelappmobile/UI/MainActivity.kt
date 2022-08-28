19734291c32115c373701152380b09f9a3f84382
AtanasNankinski <atanas.nankinski98@gmail.com>
2022-08-28 18:09:38 +0300

    feature/TriedImplementingDrawerMenu
    Tried implementing drawer menu but failed and need to refactor.

--- app/src/main/java/com/example/hotelappmobile/UI/MainActivity.kt
+++ app/src/main/java/com/example/hotelappmobile/UI/MainActivity.kt
@@ -1,17 +1,51 @@
 package com.example.hotelappmobile.UI
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.hotelappmobile.R
 import com.example.hotelappmobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var listener: NavController.OnDestinationChangedListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-
         setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        drawerLayout = binding.root
+        val sideMenu = binding.nvDrawer
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        binding.nvDrawer.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        listener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
+            if (destination.id == R.id.loginFragment){
+                sideMenu.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
